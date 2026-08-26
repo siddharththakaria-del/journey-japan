826a718559b2c9b2a2ef92563c8d80c5da2a6e48
Siddharth Thakaria <siddharththakaria@gmail.com>
2026-08-27 01:40:08 +0530

    Configure GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://journey-japan.org',
+  site: 'https://siddharththakaria-del.github.io',
+  base: '/journey-japan',
   markdown: {
     syntaxHighlight: 'shiki'
   }
